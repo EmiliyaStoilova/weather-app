c748aadb1b52675779e2f7a9f91e3b750024c795
Emiliya Stoilova <emiliya@browserbite.io>
2023-09-27 16:25:23 +0300

    page structur

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DayDetailsPage, MainPage } from "./pages";
+import { ROUTE_CONFIG } from "./routes/config";
 
 function App() {
   // console.log(process.env.REACT_APP_APPID)
   return (
     <Provider store={store}>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainPage />} path={ROUTE_CONFIG.MAIN} />
+          <Route element={<DayDetailsPage />} path={ROUTE_CONFIG.DAY_DETAIL} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
---
+++ src/routes/config.ts
@@ -0,0 +1,4 @@
+export const ROUTE_CONFIG = {
+  MAIN: "/app",
+  DAY_DETAIL: "/app/:id"
+};
